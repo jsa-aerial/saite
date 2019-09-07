a6ed0316ac7804c03e28a5e14d92a72b28322a44
jsa-aerial <jsa.aerial@gmail.com>
2019-09-06 20:50:25 -0400

    Add safty macro try+ for all user-msg code

--- src/clj/aerial/saite/core.clj
+++ src/clj/aerial/saite/core.clj
@@ -19,6 +19,35 @@
             [aerial.hanami.data :as hd]))
 
 
+(defmacro try+ [msg & body]
+  `(let [name# (get-in ~msg [:data :uid :name])]
+     (try
+       ~@body
+       (catch Error e#
+         (hmi/send-msg
+          name#
+          {:op :error
+           :data {:error (str (type e#)),
+                  :msg (->  ((Throwable->map e#) :cause)
+                            (cljstr/split #"\n") first
+                            (cljstr/split #": ") rest
+                            (->> (cljstr/join ": ")))}}))
+       (catch clojure.lang.ExceptionInfo e#
+         (hmi/send-msg
+          name#
+          {:op :error
+           :data {:error (str (type e#))
+                  :msg (str (.getMessage e#) ": " (.getData e#))}}))
+       (catch Exception e#
+         (hmi/send-msg
+          name#
+          {:op :error
+           :data {:error (str (type e#)),
+                  :cause (str ((Throwable->map e#) :cause))
+                  :msg (-> e# Throwable->map :via first :message)}})))))
+
+
+
 
 (defn xform-cljform
   [clj-form]
@@ -93,31 +122,81 @@
 
 
 
+(defmethod hmi/user-msg :set-namespace [msg]
+  (let [nsinfo (msg :data)
+        {:keys [nssym requires]} nsinfo
+        requires-stg (if (not (seq requires))
+                       ""
+                       (cljstr/join "\n                " requires))]
+    (try+ msg
+     (eval
+      (read-string
+       (format
+        "(ns %s
+      (:require [clojure.string :as str]
+                [clojure.data.csv :as csv]
+                [clojure.data.json :as json]
+
+                [com.rpl.specter :as sp]
+
+                [aerial.fs :as fs]
+                [aerial.utils.string :as astr]
+                [aerial.utils.io :refer [letio] :as io]
+                [aerial.utils.coll :refer [vfold] :as coll]
+                [aerial.utils.math :as m]
+                [aerial.utils.math.probs-stats :as p]
+                [aerial.utils.math.infoth :as it]
+
+                [aerial.hanami.common :as hc :refer [RMV]]
+                [aerial.hanami.templates :as ht]
+                [aerial.hanami.core :as hmi]
+                [aerial.hanami.data :as hd]
+                %s))"
+        nssym requires-stg))))))
+
+
+(defmethod hmi/user-msg :eval-clj [msg]
+  (try+ msg
+   (let [codeinfo (msg :data)
+         {:keys [uid nssym code]} codeinfo
+         nssym (if (string? nssym) (symbol nssym) nssym)
+         code (if (string? code) (read-string code) code)
+         res (binding [*ns* (find-ns nssym)]
+               (eval code))
+         msg {:op :evalres :data res}]
+     (hmi/send-msg (uid :name) msg))))
+
+
+
+
 (defmethod hmi/user-msg :save-doc [msg]
-  (let [{:keys [loc info]} (msg :data)
-        [_ & data] info
-        {:keys [session file]} loc
-        config (hmi/get-adb [:saite :cfg])
-        saveloc (config :saveloc)
-        dir (fs/join (fs/fullpath saveloc) session)
-        filespec (fs/join dir file)]
-    (hmi/printchan :Saving filespec)
-    (fs/mkdirs dir)
-    (binding [*print-length* nil]
-      (io/with-out-writer filespec
-        (prn (vec data))))))
+  (try+ msg
+   (let [{:keys [loc info]} (msg :data)
+         [_ & data] info
+         {:keys [session file]} loc
+         config (hmi/get-adb [:saite :cfg])
+         saveloc (config :saveloc)
+         dir (fs/join (fs/fullpath saveloc) session)
+         filespec (fs/join dir file)]
+     (hmi/printchan :Saving filespec)
+     (fs/mkdirs dir)
+     (binding [*print-length* nil]
+       (io/with-out-writer filespec
+         (prn (vec data)))))))
+
 
 (defmethod hmi/user-msg :load-doc [msg]
-  (let [config (hmi/get-adb [:saite :cfg])
-        saveloc (config :saveloc)
-        {:keys [uid location]} (msg :data)
-        {:keys [session file url]} location
-        dir (fs/join (fs/fullpath saveloc) session)
-        file (fs/join dir file)
-        data (->> (if url url file) slurp read-string)
-        msg {:op :load-doc :data data}]
-    (hmi/printchan :Loading (if url url file))
-    (hmi/send-msg (uid :name) msg)))
+  (try+ msg
+   (let [config (hmi/get-adb [:saite :cfg])
+         saveloc (config :saveloc)
+         {:keys [uid location]} (msg :data)
+         {:keys [session file url]} location
+         dir (fs/join (fs/fullpath saveloc) session)
+         file (fs/join dir file)
+         data (->> (if url url file) slurp read-string)
+         msg {:op :load-doc :data data}]
+     (hmi/printchan :Loading (if url url file))
+     (hmi/send-msg (uid :name) msg))))
 
 
 #_{:op :read-data
@@ -126,14 +205,19 @@
           :from <:url or :file
           :path the-full-path}}
 (defmethod hmi/user-msg :read-data [msg]
-  (let [{:keys [uid chankey from path]} (msg :data)
-        ftype (fs/ftype path)
-        data (cond (= from :file) (hd/get-data (fs/fullpath path))
-                   (= ftype "json") (-> path slurp json/read-str)
-                   (= ftype "csv") (-> path slurp csv/read-csv)
-                   :else (slurp path))
-        msg {:op :data :data {:chankey chankey :data data}}]
-    (hmi/send-msg (uid :name) msg)))
+  (try+ msg
+   (let [{:keys [uid chankey from path]} (msg :data)
+         ftype (fs/ftype path)
+         data (cond (= from :file) (hd/get-data (fs/fullpath path))
+                    (= ftype "json") (-> path slurp json/read-str)
+                    (= ftype "csv") (-> path slurp csv/read-csv)
+                    :else (slurp path))
+         data (if data data :BAD-DATASET)
+         msg {:op :data :data {:chankey chankey :data data}}]
+     (hmi/send-msg (uid :name) msg)
+     (when (= data :BAD-DATASET)
+       (throw
+        (ex-info (format "Bad file type or empty dataset, %s" path) {}))))))
 
 
 
