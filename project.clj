68e7e74b2dda345cf37f63005ab87c6265291707
jsa-aerial <jsa.aerial@gmail.com>
2019-09-21 12:38:51 -0400

    Bump version for paredit fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "0.12.7"
+(defproject aerial.saite "0.12.9"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
