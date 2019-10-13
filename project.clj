e21259fcd9bdd4512cf2830eb8c081f0a73999c8
jsa-aerial <jsa.aerial@gmail.com>
2019-10-13 14:03:03 -0400

    Move to hanami 0.10.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "0.17.0"
+(defproject aerial.saite "0.17.1"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
@@ -12,7 +12,7 @@
 
                  [com.rpl/specter "1.1.1"]
 
-                 [aerial.hanami "0.10.11"]
+                 [aerial.hanami "0.10.12"]
                  [cljsjs/codemirror "5.40.2-1"]
                  [cljsjs/highlight "9.12.0-2"]
                  [cljsjs/mathjax "2.7.5-0"]
