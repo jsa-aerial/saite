3dded5a236b57f04c181a2a0a46c3a26824d7078
jsa-aerial <jsa.aerial@gmail.com>
2020-02-06 20:22:15 -0500

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "0.19.7"
+(defproject aerial.saite "0.19.9"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
