534655692c9c10111017f3061a61850476eb8d20
jsa-aerial <jsa.aerial@gmail.com>
2020-02-07 16:29:40 -0500

    Bump version for cm fid fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "0.19.9"
+(defproject aerial.saite "0.19.11"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
