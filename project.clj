042c4509eb92c02321838f18e1d2f230cd562bc4
jsa-aerial <jsa.aerial@gmail.com>
2020-02-11 12:30:44 -0500

    Bump version for bulk chart fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "0.19.13"
+(defproject aerial.saite "0.19.15"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
