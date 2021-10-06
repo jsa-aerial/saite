02ef27c5fbd366cc5ecfb59a44bf313ffddf5fee
jsa-aerial <jsa.aerial@gmail.com>
2021-10-06 18:30:20 -0400

    Use latest Hanami and bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "0.25.17"
+(defproject aerial.saite "0.25.19"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
@@ -19,7 +19,7 @@
                  [clj-commons/fs "1.5.2"]             ; zip/tar
                  [com.rpl/specter "1.1.3"]
 
-                 [aerial.hanami "0.12.7"]
+                 [aerial.hanami "0.12.9"]
                  [cljsjs/codemirror "5.40.2-1"]
                  [cljsjs/highlight "9.12.0-2"]
                  [cljsjs/mathjax "2.7.5-0"]
