fe14b7cfc991e80d34a9f9c45aa206c8122a51c9
jsa-aerial <jsa.aerial@gmail.com>
2021-11-24 14:29:51 -0500

    Bump version for new tab body editors functionality

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "1.0.1"
+(defproject aerial.saite "1.1.0"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
