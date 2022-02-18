c947ea4bc2c9d51236530157cf8335b2ad102b27
jsa-aerial <jsa.aerial@gmail.com>
2022-02-18 16:22:39 -0500

    Bump version for bug fixes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.saite "1.4.3"
+(defproject aerial.saite "1.4.4"
   :description "Interactive document and visualization system - exploration, presentation, publication"
   :url "https://github.com/jsa-aerial/saite"
   :license {:name "MIT"
